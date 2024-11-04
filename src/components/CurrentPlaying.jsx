cc52dc258174c60d77004b06cec2c842a34e7654
Nandaxy <indri.khaulia12@gmail.com>
2024-11-04 18:18:02 +0700

    Fix Card

--- src/components/CurrentPlaying.jsx
+++ src/components/CurrentPlaying.jsx
@@ -6,125 +6,131 @@ import ButtonPreview from "./ui/ButtonPreview";
 import NoMusicPlaying from "./ui/NoMusicPlaying";
 
 const CurrentPlaying = () => {
-  const [musik, setMusik] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentTime, setCurrentTime] = useState(0);
+    const [musik, setMusik] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentTime, setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    const getDataMusik = async () => {
-      const res = await fetchDataSpoty("now-playing");
-      if (res.isPlaying === true) {
-        setMusik(res);
-        setCurrentTime(res.progress_ms);
-      } else {
-        setMusik(null);
-      }
-      setIsLoading(false);
+    useEffect(() => {
+        const getDataMusik = async () => {
+            const res = await fetchDataSpoty("now-playing");
+            if (res.isPlaying === true) {
+                setMusik(res);
+                setCurrentTime(res.progress_ms);
+            } else {
+                setMusik(null);
+            }
+            setIsLoading(false);
+        };
+
+        getDataMusik();
+
+        const interval = setInterval(() => {
+            getDataMusik();
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        const timeInterval = setInterval(() => {
+            if (musik && musik.isPlaying) {
+                setCurrentTime(prevTime => {
+                    const newTime = prevTime + 1000;
+                    return newTime < musik.duration_ms
+                        ? newTime
+                        : musik.duration_ms;
+                });
+            }
+        }, 1000);
+
+        return () => clearInterval(timeInterval);
+    }, [musik]);
+
+    const formatTime = ms => {
+        const seconds = Math.floor(ms / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
     };
 
-    getDataMusik();
-
-    const interval = setInterval(() => {
-      getDataMusik();
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const timeInterval = setInterval(() => {
-      if (musik && musik.isPlaying) {
-        setCurrentTime((prevTime) => {
-          const newTime = prevTime + 1000;
-          return newTime < musik.duration_ms ? newTime : musik.duration_ms;
-        });
-      }
-    }, 1000);
-
-    return () => clearInterval(timeInterval);
-  }, [musik]);
-
-  const formatTime = (ms) => {
-    const seconds = Math.floor(ms / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
-  };
-
-  return (
-    <div className="min-h-80 p-4">
-      {isLoading ? (
-        <LoadingCurrentPlaying />
-      ) : musik === null ? (
-        <NoMusicPlaying />
-      ) : (
-        <div className="mx-auto bg-gray-900 rounded-lg shadow-2xl max-w-md">
-          <div className="">
-            <img
-              src={musik.albumImageUrl}
-              alt={musik.title}
-              className="w-full h-52 md:h-60 object-cover rounded-t-lg"
-            />
-          </div>
-          <div className="p-4">
-            <div className="flex justify-between items-center mb-1">
-              <a
-                href={musik.songUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white font-bold text-lg mb-1 hover:underline truncate block"
-              >
-                {musik.name}
-              </a>
-              <ButtonPreview musik={musik} />
-            </div>
-            <div className="flex justify-between items-center mb-4">
-              {/* <p className="text-gray-400 text-sm truncate flex-1">
+    return (
+        <div className="min-h-80 p-4">
+            {isLoading ? (
+                <LoadingCurrentPlaying />
+            ) : musik === null ? (
+                <NoMusicPlaying />
+            ) : (
+                <div className="mx-auto bg-gray-900 rounded-lg shadow-2xl max-w-md">
+                    <div className="">
+                        <img
+                            src={musik.albumImageUrl}
+                            alt={musik.title}
+                            className="w-full h-52 md:h-60 object-cover rounded-t-lg"
+                        />
+                    </div>
+                    <div className="p-4">
+                        <div className="flex justify-between items-center mb-1">
+                            <a
+                                href={musik.songUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-white font-bold text-lg mb-1 hover:underline truncate block"
+                            >
+                                {musik.name}
+                            </a>
+                            <ButtonPreview musik={musik} />
+                        </div>
+                        <div className="flex justify-between items-center mb-4">
+                            {/* <p className="text-gray-400 text-sm truncate flex-1">
                 Artis: {musik.artist}
               </p> */}
-              <div className="text-gray-300 text-sm truncate">
-                {musik.artist.map((artist, index) => (
-                  <span key={index}>
-                    <a
-                      href={artist.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="hover:underline"
-                    >
-                      Artis : {artist.name}
-                    </a>
-                    {index < musik.artist.length - 1 && ", "}
-                  </span>
-                ))}
-              </div>
-              <a
-                className="text-gray-400 text-sm truncate flex-1 text-right hover:underline"
-                href={musik.albumUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Album : {musik.album}
-              </a>
-            </div>
-            <div className="relative pt-1">
-              <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-700">
-                <div
-                  style={{
-                    width: `${(currentTime / musik.duration_ms) * 100}%`,
-                  }}
-                  className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
-                ></div>
-              </div>
-              <div className="flex justify-between text-gray-400 text-xs">
-                <span>{formatTime(currentTime)}</span>
-                <span>{formatTime(musik.duration_ms)}</span>
-              </div>
-            </div>
-          </div>
+                            <div className="text-gray-300 text-sm truncate">
+                                {musik.artist.map((artist, index) => (
+                                    <span key={index}>
+                                        <a
+                                            href={artist.url}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="hover:underline"
+                                        >
+                                            {artist.name}
+                                        </a>
+                                        {index < musik.artist.length - 1 &&
+                                            ", "}
+                                    </span>
+                                ))}
+                            </div>
+                            <a
+                                className="text-gray-400 text-sm truncate flex-1 text-right hover:underline"
+                                href={musik.albumUrl}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Album : {musik.album}
+                            </a>
+                        </div>
+                        <div className="relative pt-1">
+                            <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-700">
+                                <div
+                                    style={{
+                                        width: `${
+                                            (currentTime / musik.duration_ms) *
+                                            100
+                                        }%`
+                                    }}
+                                    className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
+                                ></div>
+                            </div>
+                            <div className="flex justify-between text-gray-400 text-xs">
+                                <span>{formatTime(currentTime)}</span>
+                                <span>{formatTime(musik.duration_ms)}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default CurrentPlaying;
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,55 +1,56 @@
 /* eslint-disable react/prop-types */
 const Card = ({ musik, index }) => {
-  return (
-    <div className="flex items-center bg-gray-600 shadow-md shadow-gray-900/30 rounded-md overflow-hidden group hover:bg-gray-700 transition-all duration-300 hover:shadow-white/10">
-      <div className="w-16 md:w-20 h-full flex-shrink-0">
-        <div className="w-full h-full relative">
-          <img
-            src={musik?.albumImageUrl}
-            alt={musik?.name}
-            className="absolute inset-0 w-full h-full object-cover group-hover:opacity-70 transition-opacity duration-300"
-          />
-        </div>
-      </div>
-      <div className="flex-1 flex items-center min-w-0 py-1 md:py-2 px-3 ">
-        <div className="flex-1 min-w-0 mr-2 md:mr-6">
-          <a
-            href={musik.songUrl}
-            className="text-white font-semibold mb-1 truncate hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {index}. {musik?.name}
-          </a>
-          <div className="text-gray-300 text-sm truncate">
-            {musik.artist.map((artist, index) => (
-              <span key={index}>
-                <a
-                  href={artist.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="hover:underline"
-                >
-                  {artist.name}
-                </a>
-                {index < musik.artist.length - 1 && ", "}
-              </span>
-            ))}
-          </div>
+    return (
+        <div className="flex items-center bg-gray-600 shadow-md shadow-gray-900/30 rounded-md overflow-hidden group hover:bg-gray-700 transition-all duration-300 hover:shadow-white/10">
+            <div className="w-16 md:w-20 h-full flex-shrink-0">
+                <div className="w-full h-full relative">
+                    <img
+                        src={musik?.albumImageUrl}
+                        alt={musik?.name}
+                        className="absolute inset-0 w-full h-full object-cover group-hover:opacity-70 transition-opacity duration-300"
+                    />
+                </div>
+            </div>
+            <div className="flex-1 flex items-center min-w-0 py-1 md:py-2 px-3">
+                <div className="flex-1 min-w-0 mr-2 md:mr-6">
+                    <a
+                        href={musik.songUrl}
+                        className="text-white font-semibold mb-1 truncate hover:underline"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ display: "block" }}
+                    >
+                        {index}. {musik?.name}
+                    </a>
+                    <div className="text-gray-300 text-sm truncate">
+                        {musik.artist.map((artist, artistIndex) => (
+                            <span key={artistIndex} className="truncate">
+                                <a
+                                    href={artist.url}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="hover:underline"
+                                >
+                                    {artist.name}
+                                </a>
+                                {artistIndex < musik.artist.length - 1 && ", "}
+                            </span>
+                        ))}
+                    </div>
 
-          <a
-            href={musik.albumUrl}
-            className="text-gray-300 text-sm  truncate hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Album : {musik?.album}
-          </a>
+                    <a
+                        href={musik.albumUrl}
+                        className="text-gray-300 text-sm truncate hover:underline block whitespace-nowrap overflow-hidden text-ellipsis"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Album: {musik?.album}
+                    </a>
+                </div>
+            </div>
+            <div className="flex-shrink-0"></div>
         </div>
-        <div className="flex-shrink-0"></div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Card;
